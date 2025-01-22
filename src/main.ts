015bd837f6a6c7bad2a2b114262bc696491780b7
Ezequiel <eolavaca@gmail.com>
2025-01-22 11:36:12 -0300

    mobile styling ready

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ const elems =  async () => {
   const request = await getProducts();
   const prods = []
   if(request.length > 0) {
-    document.querySelector<HTMLElement>('#app')!.innerHTML = `<ul id="list" class="flex flex-row flex-wrap gap-y-4 justify-between"></ul>`;
-    request.forEach((prod) => {
-      prods.push(templaterunner(prod));
+    document.querySelector<HTMLElement>('#app')!.innerHTML = `<ul id="list" class="grid grid-cols-2 md:grid-cols-4 lg:flex lg:overflow-x-auto lg:space-x-8 w-1/2"></ul>`;
+    request.forEach((prod, i) => {
+      prods.push(templaterunner(prod, i));
     });    
-    return prods;
+
   } else {
     document.querySelector<HTMLElement>('#app')!.innerHTML = `<h2>Sorry something went wrong, please try again in a couple of minutes</h2>`
   }
--- src/templaterunner.ts
+++ src/templaterunner.ts
@@ -1,26 +1,26 @@
-const templaterunner = (prod) => {
+import starsCreator from "./starsCreator";
+const templaterunner = (prod, i) => {
     const target = document.querySelector<HTMLUListElement>('#list');
-    const item = `<div class="max-w-44 -m-2 relative">
-                    <figure class="bg-orange-100">
+    const item = `<div class="relative">
+                    <figure class="bg-orange-100 rounded-lg mb-4">
                         <img src="${prod.thumbnail}" alt="">
                     </figure>
-                    <span class="absolute inset-2">Best seller</span>
-                    <span class="${prod.discountPercentage <= 10 ? 'hidden' : ''}">Save ${Math.round(prod.discountPercentage)}%</span>
-                    <h2>${prod.title}</h2>
-                    <p>
-                        <span class="">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                        <span>${prod.reviews.length} Reviews</span>
+                    <span class="absolute top-1 left-1 rounded-2xl p-1 bg-white border-2 border-black font-sans text-center text-xs">Best seller</span>
+                    <span class="absolute top-1 right-1 rounded-2xl p-1 bg-lime-900 text-white border-2 border-black font-sans text-center text-xs ${prod.discountPercentage <= 10 ? 'hidden' : ''}">Save ${Math.round(prod.discountPercentage)}%</span>
+                    <h2 class="font-sans font-semibold uppercase text-lg mb-3">${prod.title}</h2>
+                    <p class="mb-3">
+                        ${starsCreator(prod.rating)}
+                        <span class="font-sans text-gray-600 font-sm font-medium align-top">${prod.reviews.length} Reviews</span>
                     </p>             
-                    <p>$ ${prod.price}</p>
+                    <p class="font-sans font-semibold text-lg">$ ${prod.price}</p>
                 </div>`;
                 
             const tempLi = document.createElement('li');
+            tempLi.innerHTML = item;    
+            if(i >= 4)  {
+                tempLi.classList.add('hidden', 'md:block')
+            }
             
-            tempLi.innerHTML = item;          
             if (tempLi) {
                 target?.appendChild(tempLi);
             }
