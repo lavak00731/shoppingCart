1fbe88092f68a264b15d02ea0cdb49083fd67f31
Ezequiel <eolavaca@gmail.com>
2025-01-22 15:47:00 -0300

    styling desktop

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const elems =  async () => {
   const request = await getProducts();
   const prods = []
   if(request.length > 0) {
-    document.querySelector<HTMLElement>('#app')!.innerHTML = `<ul id="list" class="grid grid-cols-2 md:grid-cols-4 lg:flex lg:overflow-x-auto lg:space-x-4 lg:w-5/6 lg:mx-auto lg:"></ul>`;
+    document.querySelector<HTMLElement>('#app')!.innerHTML = `<ul id="list" class="grid grid-cols-2 md:grid-cols-4 lg:flex lg:overflow-x-auto lg:space-x-4 lg:w-7/8 lg:mx-auto"></ul>`;
     request.forEach((prod, i) => {
       prods.push(templaterunner(prod, i));
     });    
--- src/templaterunner.ts
+++ src/templaterunner.ts
@@ -5,9 +5,9 @@ const templaterunner = (prod, i) => {
                     <figure class="bg-orange-100 rounded-lg mb-4">
                         <img src="${prod.thumbnail}" alt="">
                     </figure>
-                    <span class="absolute top-1 left-1 rounded-2xl p-1 bg-white border-2 border-black font-sans text-center text-xs">Best seller</span>
-                    <span class="absolute top-1 right-1 rounded-2xl p-1 bg-lime-900 text-white border-2 border-black font-sans text-center text-xs ${prod.discountPercentage <= 10 ? 'hidden' : ''}">Save ${Math.round(prod.discountPercentage)}%</span>
-                    <h2 class="font-sans font-semibold uppercase text-lg mb-3">${prod.title}</h2>
+                    <span class="absolute uppercase top-1 left-1 lg:top-2 lg:left-2 rounded-2xl p-1 bg-white border-2 border-black font-sans text-center text-xs lg:text-sm">Best seller</span>
+                    <span class="absolute uppercase top-2 right-5 rounded-2xl p-1 bg-lime-900 text-white border-2 border-black font-sans text-center text-xs lg:text-sm ${prod.discountPercentage <= 10 ? 'hidden' : ''}">Save ${Math.round(prod.discountPercentage)}%</span>
+                    <h2 class="font-sans font-semibold uppercase text-lg lg:text-xl mb-3 lg:mb-5">${prod.title}</h2>
                     <p class="mb-3">
                         ${starsCreator(prod.rating)}
                         <span class="font-sans text-gray-600 font-sm font-medium align-top">${prod.reviews.length} Reviews</span>
@@ -20,7 +20,7 @@ const templaterunner = (prod, i) => {
             if(i >= 4)  {
                 tempLi.classList.add('hidden', 'md:block')
             }
-            tempLi.classList.add('lg:flex-shrink-0', 'lg:basis-44')
+            tempLi.classList.add('lg:flex-shrink-0', 'lg:basis-46')
             if (tempLi) {
                 target?.appendChild(tempLi);
             }
